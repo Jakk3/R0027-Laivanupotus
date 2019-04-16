8beb2fa509fd26aac348362fa9fd1bd5292d3e11
Vili Muotka <32640696+Jakk3@users.noreply.github.com>
2019-04-16 14:17:34 +0300

    Shipbuilder Part 2
    the shipbuilder seems to successfully build given ships at random positions and without them being on top of each other or offscreen

--- src/Laivanupotus.java
+++ src/Laivanupotus.java
@@ -7,16 +7,25 @@ public class Laivanupotus {
 		// declare variables and create a scanner
 		Scanner input = new Scanner(System.in);
 		int size = 10; // battle map size
+		int[]  shipLengths = { 1, 2, 3, 3, 4, 5 };
 		int shipCount = 6;
-		int  shipLength = 6;
 		char[][] sea = new char[size][size];
 		char[][][] ships = new char[shipCount][size][size];
 
-		ships = createShips(shipCount, shipLength, size);
+		ships = createShips(shipCount, shipLengths, size);
 		
+		for (int c = 0; c < shipCount; c++) {
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size; j++) {
-				if (ships[4][i][j] == '#') {
+				if (ships[c][i][j] == '#') {
+					sea[i][j] = '#';
+				}
+			}
+		}
+		}
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				if (sea[i][j] == '#') {
 					System.out.print("# \t");
 				} else {
 					System.out.print("~ \t");
@@ -24,11 +33,11 @@ public class Laivanupotus {
 			}
 			System.out.println();
 		}
-
 		
 	}
+		
 	
-	public static char[][][] createShips(int shipCount, int shipLength, int size) {
+	public static char[][][] createShips(int shipCount, int[] shipLengths, int size) {
 		// declaring local variables 
 		int x, y, dir;
 		char[][][] temp = new char[shipCount][size][size];
@@ -39,31 +48,28 @@ public class Laivanupotus {
 			// choosing random starting point and a direction for every ship
 			// checking if the ship would collide with another ship with the starting point and direction
 			do {
+				System.out.println("uudet randomit");
 				x = (int)(Math.random() * (size - 1));
 				y = (int)(Math.random() * (size - 1));
 				dir = (int)(Math.random() * 4);
 				
-			} while (checkCoordinates(x, y, dir, shipLength, temp, i)); // while coordinates dont collide
+			} while (checkCoordinates(x, y, dir, shipLengths[i],shipLengths, temp, i, size)); // while coordinates dont collide
 			
 			// then creating the ship from the given starting point with the direction
-			for (int j = 0; j < shipLength; j++) {
-				temp [i][x][y] = '#';
+			for (int j = 0; j < shipLengths[i]; j++) {
+				temp [i][y][x] = '#';
 				switch (dir) {
 				case 0: // up
-					if (y > 0 ) 
-						y--;
+					y--;
 					break;
 				case 1: // right
-					if (x < 9 ) 
-						x++;
+					x++;
 					break;
 				case 2: // down
-					if (y < 9 ) 
-						y++;
+					y++;
 					break;
 				case 3: // left
-					if (x > 0 ) 
-						x--;
+					x--;
 					break;
 				}
 			}
@@ -72,32 +78,42 @@ public class Laivanupotus {
 	}
 	
 	
-	public static boolean checkCoordinates(int x, int y, int dir, int shipLength, char[][][] temp, int shipIndex) {
+	public static boolean checkCoordinates(int x, int y, int dir, int shipLength, int[] shipLengths, char[][][] temp, int shipIndex, int size) {
 		// going through ships created before the current ship
 		for (int i = 0; i < shipLength; i++) {
 			for (int j = 0; j < shipIndex; j++) {
 				// if there is a ship block at a point where the new ship would have a ship block, return true
-				if (temp[j][x][y] == '#') {
+				if (temp[j][y][x] == '#') {
 					return true;
 				}
 			}
 			// change the x or y coordinate based on which direction the ship is build
+			System.out.println("shiplength " + shipLength);
+			System.out.println("x = " + x + " y = " + y);
 			switch (dir) {
 			case 0: // up
-				if (y > 0 ) 
+				if (y > (shipLengths[i] - 1)) 
 					y--;
+				else 
+					return true;
 				break;
 			case 1: // right
-				if (x < 9 ) 
+				if (x < (size - 1 - shipLengths[i])) 
 					x++;
+				else 
+					return true;
 				break;
 			case 2: // down
-				if (y < 9 ) 
+				if (y < (size - 1 - shipLengths[i]) ) 
 					y++;
+				else 
+					return true;
 				break;
 			case 3: // left
-				if (x > 0 ) 
+				if (x > (shipLengths[i] - 1)) 
 					x--;
+				else 
+					return true;
 				break;
 			}
 		}
